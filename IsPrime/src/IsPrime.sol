8b6e8b9abc3bb11c4f58e662286160ca99797664
Ronald Koh <ronald9872@gmail.com>
2025-02-06 16:08:08 +0800

    05-for loops

--- IsPrime/src/IsPrime.sol
+++ IsPrime/src/IsPrime.sol
@@ -5,7 +5,16 @@ contract IsPrime {
     /**
      * The goal of this exercise is to return if "number" is prime or not (true or false)
      */
-    function isPrime(uint256 number) public view returns (bool) {
+    function isPrime(uint256 number) public pure returns (bool) {
         // your code here
+        if (number <= 1) {
+            return false;
+        }
+        for (uint256 i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
