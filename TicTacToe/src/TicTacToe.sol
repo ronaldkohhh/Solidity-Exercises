6a6a69c52970b6a1be28d5ffc6baa56bd5770999
Ronald Koh <ronald9872@gmail.com>
2025-02-07 00:17:54 +0800

    7 - nested arrays

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -12,5 +12,25 @@ contract TicTacToe {
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
         // your code here
+        // Check rows
+        for (uint256 i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+        }
+        // Check columns
+        for (uint256 i = 0; i < 3; i++) {
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return true;
+            }
+        }
+        // Check diagonals
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true;
+        }
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return true;
+        }
+        return false;
     }
 }
